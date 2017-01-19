d0643b7cde9ebeaa75d20fa8f2a3b35f4df97dd4
Nawachai Chanaphan <nawachai.ch@outlook.com>
2017-01-19 14:39:48 +0700

    Initial checkin of sales tool.
    Signed-off-by: Nawachai Chanaphan<nawachai.ch@outlook.com>

---
+++ SalesTool/src/ca/demo/salestool/Main.java
@@ -0,0 +1,16 @@
+package ca.demo.salestool;
+
+public class Main	{
+	public static void main(String[] args)	{
+		SalesData data = new SalesData();
+		
+		displayGreeting();
+		data.display();
+		
+	}
+	
+	private static void displayGreeting()	{
+		System.out.println("HELLO HAPPY SALE PEOPLE!");
+		System.out.println("THIS APP SHOW SALES DATA");
+	}
+}
